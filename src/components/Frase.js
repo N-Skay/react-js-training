29bd3ead38913489666e5f34035d4f114c6db98a
Nicolas Schianti <nicoschianti@gmail.com>
2023-03-28 15:07:06 -0300

    Componente Frase.js

---
+++ src/components/Frase.js
@@ -0,0 +1,10 @@
+function Frase(){
+    return(
+        <div>
+            <p>Componente Frase!</p>
+            <h5>Este componente possui uma frase!</h5>
+        </div>
+    )
+}
+
+export default Frase
--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,6 +1,9 @@
+import Frase from "./Frase"
+
 function HelloWorld(){
 return(
     <div>
+        <Frase/>
         <h2>Componente Hello World!</h2>
     </div>
 )
