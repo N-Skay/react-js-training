fabf3964db3988327bd9e8af87a4406039448efa
Nicolas Schianti <nicoschianti@gmail.com>
2023-03-29 09:34:29 -0300

    Css do componente Frase.js.

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,7 +1,9 @@
+import styles from './Frase.module.css';
+
 function Frase(){
     return(
-        <div>
-            <p>Componente Frase!</p>
+        <div className={styles.fraseContainer}>
+            <p className={styles.fraseContent}>Componente Frase!</p>
             <h5>Este componente possui uma frase!</h5>
         </div>
     )
