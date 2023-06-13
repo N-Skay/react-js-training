06642b2359b683522d268d4ac2aeae4c6ebe3bdb
Nicolas Schianti <nicoschianti@gmail.com>
2023-06-12 23:11:50 -0300

    Eventos por Props

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,14 +1,28 @@
+import  Button from './eventos/Button'
+
 function Evento({numero}){
 
     function meuEvento(){
         console.log(`Fui ativado! ${numero}`)
     }
+
+    function meuEventoDois(){
+        console.log(`Evento ativado!`)
+    }
+
+    function meuEventoTres(){
+        console.log(`Terceiro evento ativado!`)
+    }
+
+
     return(
         <div>
             <p>Clique para disparar um evento:</p>
             <button onClick={meuEvento}>Ativar</button>
+            <Button event={meuEventoDois} text="Primeiro Evento"/>
+            <Button event={meuEventoTres} text="Outro Evento"/>
         </div>
-    )
+    )       
 }
-
+ 
 export default Evento
